cd1c899e84ddd26381527e7cf340949610c80f31
Bielzinmilgrau <108942711+Bielzinmilgrau@users.noreply.github.com>
2022-09-30 12:00:28 -0300

    30/09

---
+++ estudo.js
@@ -0,0 +1,13 @@
+let tabuada = 2;
+ function escreva(){
+   alert("")
+   //2 x 1 = 2
+   document.write(tabuada + " x 1 = "+(tabuada*1))+"br");
+    document.write(tabuada + " x 2 = "+(tabuada*2))+"br");
+       document.write(tabuada + " x 3 = "+(tabuada*3))+"br");
+         document.write(tabuada + " x 4 = "+(tabuada*4))+"<br>");
+            document.write(tabuada + " x 5 = "+(tabuada*5))+"<br>");
+               document.write(tabuada + " x 6 = "+(tabuada*6))+"<br>");
+                  document.write(tabuada + " x 7 = "+(tabuada*7))+"<br>");
+
+ }
