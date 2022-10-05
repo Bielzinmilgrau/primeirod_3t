62da140c8ce4bf14b5e9e2bc8fe133b6e01ca50c
Bielzinmilgrau <108942711+Bielzinmilgrau@users.noreply.github.com>
2022-10-05 11:11:05 -0300

    05/10

--- estudo.js
+++ estudo.js
@@ -1,13 +1,24 @@
 let tabuada = 2;
- function escreva(){
-   alert("")
+ function escreva() {
    //2 x 1 = 2
-   document.write(tabuada + " x 1 = "+(tabuada*1))+"br");
-    document.write(tabuada + " x 2 = "+(tabuada*2))+"br");
-       document.write(tabuada + " x 3 = "+(tabuada*3))+"br");
-         document.write(tabuada + " x 4 = "+(tabuada*4))+"<br>");
-            document.write(tabuada + " x 5 = "+(tabuada*5))+"<br>");
-               document.write(tabuada + " x 6 = "+(tabuada*6))+"<br>");
-                  document.write(tabuada + " x 7 = "+(tabuada*7))+"<br>");
-
+   document.write(tabuada + " x 1 = "+(tabuada*1)+"br");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"br");
+       document.write(tabuada + " x 3 = "+(tabuada*3)+"br");
+         document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+            document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+               document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+                  document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
  }
+  function repete() {
+    for( let i= 1; i <= 10; i++){
+      document.write("Tabuada do" +i+ "<br>");
+       for( let i= 1; j <= 10; j++){
+       document.write( i + " x " + j + " = "+(j*i)+"<br>");
+    }
+  document.write("<br>");
+   }
+}
+let lista = [ "Banana","uva","Abacate","Tomate"]
+function minhaLista(){
+   document.write(lista[3]);
+}
