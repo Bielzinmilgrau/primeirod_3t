40e5df7ff1b4780114c204009cfcb5f45df41e08
Bielzinmilgrau <108942711+Bielzinmilgrau@users.noreply.github.com>
2022-11-09 10:59:35 -0300

    09/11

--- estudo.js
+++ estudo.js
@@ -1,43 +1,38 @@
-function moeda(atual){
-return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+function moeda(atual) {
+    return atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 }
 
-function total(){
-  let c = document.getElementById("valor").value;
-  let j = document.getElementById("juros").value;
-  let t = document.getElementById("meses").value;
-  let r = moeda;
+function total() {
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = c;
+    if (!Number(c)) {
+        alert("O capital deve ser numeros.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return;
+    }
+    if (!Number(j)) {
+        alert("O juros deve ser numeros.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return;
+    }
+    if (!Number(t)) {
+        alert("O meses deve ser numeros.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return;
+    }
+    let mes = "";
+    for (let i = 1; i <= t; i++) {
+        r = c * (1 + (j / 100));
+        mes += i + ": " + moeda(r) + "<br>";
+        //  document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
+        c = r;
+    }
+    document.getElementById("mes").innerHTML = mes;
+    document.getElementById("total").innerHTML = "Total: " + moeda(r);
+    // document.write("Resultado: " + moeda(r));
 
-
-  if(!Number(c)){
-      
-   alert("O capital deve ser numeros.");
-   document.getElementById("valor").value = "";
-   document.getElementById("valor").focus();
-   return;
-  }
-
-  if(!Number(j)){   
-    alert("O juros deve ser numeros.");
-    document.getElementById("juros").value = "";
-    document.getElementById("juros").focus();
-    return;
-   }
-
-   if(!Number(t)){
-    alert("O meses deve ser numeros.");
-    document.getElementById("meses").value = "";
-    document.getElementById("meses").focus();
-    return;
-   }
-  }
-
-  for(let i = 1; i <= t; i++){
-      r = c * (1 + (j / 100));
-      document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
-      c = r ;
-  }
-  document.getElementById("total").innerHTML="Total: "+moeda(r);
-  //document.write("Resultado: " + moeda(r));
- document.getElementById(" dia 01 matando aula")
- h1< EU ESTOU TROCANDO LETRAS PARA GANHAR COMITS/>
